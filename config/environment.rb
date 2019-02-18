0c4a31a9a47a310cc842fe6a7d027382e2eb219c
Victoria B <viktorija.bourassa@gmail.com>
2019-02-18 21:36:43 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,16 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
 # put the code to connect to the database here
 
 
